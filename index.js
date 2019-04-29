dbda6bdbb123e12012146af17c929886a8034bde
maribel-arac <maribel.arca@gmail.com>
2019-04-29 12:16:42 -0500

    se agregaron las funciones para verificar un archivo, saber si existe la ruta, y si tiene una extension MD

--- index.js
+++ index.js
@@ -1,3 +1,92 @@
-module.exports = () => {
-  // ...
+const fs = require('fs');
+const path = require('path');
+
+
+module.exports = {
+
+//función para verificar si esta el archivo(el campo esta vacío o completo)
+pathInserted: function(pathFile) {
+if(pathFile != undefined) {
+	console.log("true");
+	return true
+}
+else {
+	return false
+}
+},
+
+//función para saber si existe la ruta
+pathExists: function(pathFile) {
+	if(fs.existsSync(pathFile)){
+		console.log("true")
+		return true
+	} else {
+		console.log("false");
+		return false
+	}
+	},
+
+//función para verificar si la ruta es un directorio
+pathDirectory: function(pathFile){
+  if(fs.statSync(pathFile).isDirectory()){
+    return true
+  } else{
+    return false
+  }
+},
+
+//función para saber si tiene una extension MD
+mdExtension: function (pathFile){
+		if (path.extname(pathFile) == ".md") {
+			return true
+		} else {
+			return false
+		}
+	},
+
+	// Para leer el archivo 
+	// readingFileMD: function(pathFile) {
+	// let file = fs.readFileSync(pathFile, 'utf-8');
+	// console.log(file);
+	// return true;
+	// },
+
+//leer el archivo para saber si tiene extensión MD (asincrono con una promesa)
+readFileMd : function(pathFile, options){
+return new Promise((resolve, reject)=> {
+  // Leer el archivo
+  fs.readFile(pathFile, function(err, data){
+    if (err){
+      return reject(err);
+    }
+    resolve(data.toString());
+  });
+});
+},
+
+
+//función para saber si contiene md-links
+	containsMdLinks:() => {
+
+	},
+
+//para extraer el link
+urlify: function(data) {
+  // console.log(txt);
+  const mdLinkRgEx = /\[(.+?)\]\(.+?\)/g;
+  const mdLinkRgEx2 = /\[(.+?)\]\((.+?)\)/;
+
+  let allLinks = data.match(mdLinkRgEx);
+  var htmlLinks = [];
+  for (var x in allLinks) {
+    var grpdDta = mdLinkRgEx2.exec(allLinks[x]);
+    var linkified = "<a href=\"" + grpdDta[2] + "\">" + grpdDta[1] + "<a>";
+    console.log(linkified);
+    htmlLinks.push(linkified);
+  }
+  return htmlLinks;
+}
+
 };
+
+console.log(module.exports.urlify("./README.md"));
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,10 +1,51 @@
-const mdLinks = require('../');
+const mdLinks = require('../index.js');
 
 
 describe('mdLinks', () => {
+	it('is an object',()=>{
+		expect(typeof mdLinks).toBe('object');
 
-  it('should...', () => {
-    console.log('FIX ME!');
+	});
+});
+
+describe('pathInserted', () => {
+	it('should be a function',()=>{
+		expect(typeof mdLinks.pathInserted).toBe('function'); //mdLinks es el nombre de la función 
+	});
+});
+
+describe('pathExists', () => {
+	it('should be a function',()=>{
+		expect(typeof mdLinks.pathExists).toBe('function');  
+	});
+});
+
+describe('pathDirectory', () => {
+	it('should be true', () => {
+		expect(mdLinks.pathDirectory("/Users/Maribel/Desktop/GDL002-md-links")).toBe(true);
+	});
+});
+
+describe('mdExtension', () => {
+	it('should be a function',()=>{
+		expect(typeof mdLinks.mdExtension).toBe('function');  
+	});
+});
+
+describe('readFileMD', () => {
+	it("Should read the file", () => {
+		expect(mdLinks.readFileMd("prueba.md")).toBe(true);
+	});
+});
+
+describe('containsMdLinks', () => {
+	it('should be a function', () => {
+		expect(typeof mdLinks.containsMdLinks).toBe('function');
+	});
+})
+
+describe("urlify", () =>{
+  it("Should be true", ()=> {
+    expect(mdLinks.urlify("./README.md")).toBe(htmlLinks);
   });
-
 });
